372ae5b3c73f8e081df2ea63d8e3fd867b90c1d3
Zazixxs <125758932+Zazixxs@users.noreply.github.com>
2025-01-19 22:44:47 +0100

    update

---
+++ Python/app.py
@@ -0,0 +1,43 @@
+from flask import Flask , render_template
+from bs4 import BeautifulSoup
+import requests
+
+
+
+
+def getSchedule():
+    url = "https://cloud.timeedit.net/hda/web/public/s/s.html?tab=3&object=person.stud.h22carja&type=Alla&object=courseOccasion.H3J6K&type=Alla&object=courseOccasion.H3KAY&type=Alla&object=courseOccasion.V3KPR&type=Alla&object=courseOccasion.V3KPS&type=Alla&object=programmeOccasion.H3BCW&type=Alla&p=6.months"
+    response = requests.get(url)
+    
+    if response.status_code != 200:
+        return "fail"
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    events = []
+    for tr in soup.find_all('tr', class_='clickable2'):
+        event_data = {
+            'tid': tr.find('td', class_='time').text.strip(),
+            'koder': tr.find('td', class_='column0').text.strip(),
+            'namn': tr.find('td', class_='column1').text.strip(),
+            'typ': tr.find_all('td', class_='column0')[2].text.strip(),
+            'ledare': tr.find('td', class_='column1', id=None).text.strip(),  # Senare förekomst av column1 utan id
+            'plats': tr.find_all('td', class_='column0')[-1].text.strip(),
+        }
+        events.append(event_data)
+
+
+        print()
+
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
+
+
